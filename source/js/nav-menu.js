a5e39cd8ffd1a5e23eff326f2c5f401a4b1b4d6f
SharpFox <roman.bb.vishnevsky@gmail.com>
2018-05-15 17:31:34 +0300

    final_check-2 #1

--- source/js/nav-menu.js
+++ source/js/nav-menu.js
@@ -5,15 +5,17 @@
     var MAIN_NAV_OPEN_CLASS = 'main-nav--open';
     var MAIN_NAV_TOGGLE_CLASS = 'main-nav__toggle';
     var MAIN_NAV_TOGGLE_OPEN_CLASS = 'main-nav__toggle--open';
-    var MAIN_NAV_NOJS_CLASS = 'main-nav--nojs';
-    var MAIN_NAV_TOGGLE_NOJS_CLASS = 'main-nav__toggle--nojs';
     var MAIN_NAV_ITEM_LOGO_OPEN_CLASS = 'main-nav__item--logo-open';
+    var MAIN_NAV_NOJS_CLASS = 'main-nav--nojs';
+    var MAIN_NAV_ITEM_NOJS_CLASS = 'main-nav__item--nojs';
+    var MAIN_NAV_TOGGLE_NOJS_CLASS = 'main-nav__toggle--nojs';
 
     var navMain = document.querySelector('.' + MAIN_NAV_CLASS);
     var navMainItemLogo = navMain.querySelector('.' + MAIN_NAV_ITEM_LOGO_OPEN_CLASS);
     var navToggle = document.querySelector('.' + MAIN_NAV_TOGGLE_CLASS);
 
     navMain.classList.remove(MAIN_NAV_NOJS_CLASS);
+    navMainItemLogo.classList.remove(MAIN_NAV_ITEM_NOJS_CLASS);
     navToggle.classList.remove(MAIN_NAV_TOGGLE_NOJS_CLASS);
 
     navToggle.addEventListener('click', function () {
@@ -21,7 +23,7 @@
             navMain.classList.remove(MAIN_NAV_OPEN_CLASS);
             navToggle.classList.remove(MAIN_NAV_TOGGLE_OPEN_CLASS);
             navMainItemLogo.classList.add(MAIN_NAV_ITEM_LOGO_OPEN_CLASS);
-
+            
             return;
         }
 
